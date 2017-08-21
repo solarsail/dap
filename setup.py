dc3fc6e86f5a5b5e0dbb6b5281196b451a148938
solarsail <newleaf.lu@gmail.com>
2017-08-21 15:51:09 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "sdap",
-    version = "0.2.4",
+    version = "0.2.5",
     author = "solarsail",
     author_email = "newleaf.lu@gmail.com",
     packages = ["sdap", "sdap.api"],
